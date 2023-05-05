3e76cafe41ca0c56c7fa072c202b869c7830b6a7
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-05-05 02:20:45 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Mariam Kasim All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: May
 // This file contains the JS functions for index.html
+
+"use strict"
+
+// Function to recommend a movie based on age group
+function myButtonClicked() {
+  // Input
+  const age = parseInt(document.getElementById("age").value)
+
+  // Process
+  let movie = ""
+  if ((age = Toddler)) {
+  movie = "Barney"
+  } else if ((age = PreTeen)) {
+  movie = "Harry Potter"
+} else if ((age = Teen)) {
+  movie = "Twilight"
+} else if ((age = Adult)) {
+  movie = "The Godfather"
+}
+
+// Output
+document.getElementById("answer").innerHTML =
+  "You should watch " + movie + " Movie" }
